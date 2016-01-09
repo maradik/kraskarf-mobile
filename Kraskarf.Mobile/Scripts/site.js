e8a3ca71fe7731476eb7cdd5880a2b037d1dcf1c
maradik <maradik@bk.ru>
2016-01-09 05:16:27 +0500

    Basic view for Catalog of products

--- Kraskarf.Mobile/Scripts/site.js
+++ Kraskarf.Mobile/Scripts/site.js
@@ -1,9 +1,33 @@
-﻿var initSmoothScroll = function () {
+﻿$(function() {
     $('a.smooth').click(function() {
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top - 80
         }, 500);
     });
-}
+});
 
-initSmoothScroll();
+$(function () {
+
+    $(".numbers-row").append('<div class="inc button_incdec">+</div><div class="dec button_incdec">-</div>');
+
+    $(".button_incdec").on("click", function () {
+
+        var $button = $(this);
+        var oldValue = $button.parent().find("input").val();
+
+        if ($button.text() == "+") {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+
+        $button.parent().find("input").val(newVal);
+
+    });
+
+});
